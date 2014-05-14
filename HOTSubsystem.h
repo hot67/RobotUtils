93130244d4e4ffa817da425cde3f13f5a3a89735
hal7df <hal7df@gmail.com>
2014-05-14 08:42:19 -0400

    Removed friendship from HOTSubsystem, considering that it is not inherited.

--- HOTSubsystem.h
+++ HOTSubsystem.h
@@ -1,9 +1,17 @@
+/* HOTSubsystem
+ *
+ * This is the base class for all
+ * objects to be used with
+ * SubsystemHandler. Note: All classes
+ * that inherit this must be a friend
+ * of the SubsystemHandler so it can call
+ * the update() function.
+ */
+
 #ifndef HOTSUBSYSTEM_H
 #define HOTSUBSYSTEM_H
 
 class HOTSubsystem {
-public:
-    //friend class SubsystemHandler;
 
 protected:
     virtual void update();
