ac82198c5b0cb64c197bd075ce9336828ccaf97b
hal7df <hal7df@gmail.com>
2014-05-14 08:38:48 -0400

    Forgot to commit the HOTSubsystem class.

--- HOTSubsystem.h
+++ HOTSubsystem.h
@@ -1,4 +1,12 @@
 #ifndef HOTSUBSYSTEM_H
 #define HOTSUBSYSTEM_H
 
+class HOTSubsystem {
+public:
+    //friend class SubsystemHandler;
+
+protected:
+    virtual void update();
+};
+
 #endif // HOTSUBSYSTEM_H
