aeccba1cd348714de7e845779782dcefad8f2ef8
hal7df <hal7df@gmail.com>
2015-01-17 12:43:06 -0500

    REALLY REALLY updated enums to match new controls system.

--- AdvancedJoystick.h
+++ AdvancedJoystick.h
@@ -32,18 +32,18 @@ public:
 
     // BUTTON AND AXIS ENUMS ----------
     typedef enum {
-        kButtonA = 0,
-        kButtonB = 1,
-        kButtonX = 2,
-        kButtonY = 3,
-        kButtonLB = 4,
-        kButtonRB = 5,
-        kButtonBack = 6,
-        kButtonStart = 7,
-        kButtonL3 = 8,
-        kButtonR3 = 9,
-        kTriggerL = 10,
-        kTriggerR = 11
+        kButtonA = 1,
+        kButtonB = 2,
+        kButtonX = 3,
+        kButtonY = 4,
+        kButtonLB = 5,
+        kButtonRB = 6,
+        kButtonBack = 7,
+        kButtonStart = 8,
+        kButtonL3 = 9,
+        kButtonR3 = 10,
+        kTriggerL = 11,
+        kTriggerR = 12
     } button_t;
 
     typedef enum {
@@ -57,11 +57,10 @@ public:
     typedef enum {
         kLeftX = 0,
         kLeftY = 1,
-        kRawTrigger = 2,
-        kRightX = 3,
-        kRightY = 4,
-        kLeftTrigger = 5,
-        kRightTrigger = 6
+        kLeftTrigger = 2,
+        kRightTrigger = 3,
+        kRightX = 4,
+        kRightY = 5,
     } axis_t;
 
     //CONSTRUCTORS --------------
