fe776e6b39ce6d67171aa855976ebea2589b80dd
jyori112 <jyori112@gmail.com>
2016-01-22 16:29:54 -0500

    Add .h files to RobotUtils.h

--- RobotUtils.h
+++ RobotUtils.h
@@ -2,12 +2,14 @@
 #ifndef SRC_ROBOTUTILS_H_
 #define SRC_ROBOTUTILS_H_
 
+#include "WPILib.h"
 #include "HotBot.h"
 #include "HotJoystick.h"
 #include "HotLogChannel.h"
+#include "HotLogFile.h"
+#include "HotLogger.h"
+#include "HotLogSystem.h"
 #include "HotPIDController.h"
 #include "HotSubsystem.h"
-
-
 
 #endif /* SRC_ROBOTUTILS_H_ */
