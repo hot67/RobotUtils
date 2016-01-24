627ef86dd4a6b260143ef398799965e98a9801f8
jyori112 <jyori112@gmail.com>
2016-01-23 21:26:39 -0500

    Add Dynamic PID

--- HotPIDController.cpp
+++ HotPIDController.cpp
@@ -4,15 +4,20 @@
 /******************************
  * 	Constructors
  ******************************/
-
 HotPIDController::HotPIDController(float p, float i, float d, PIDSource *source, PIDOutput *output, float period)
 : PIDController(p, i, d, source, output, period) {
 	m_source = source;
+	def_p = p;
+	def_i = i;
+	def_d = d;
 }
 
 HotPIDController::HotPIDController(float p, float i, float d, float f, PIDSource *source, PIDOutput *output, float period)
 : PIDController(p, i, d, f, source, output, period) {
 	m_source = source;
+	def_p = p;
+	def_i = i;
+	def_d = d;
 }
 
 /******************************
@@ -57,6 +62,22 @@ double HotPIDController::GetOutput() const {
 /******************************
  * 	Control PID
  ******************************/
+/**
+ * 	Get Default PID
+ */
+float HotPIDController::GetDefP() {
+	return def_p;
+}
+float HotPIDController::GetDefI() {
+	return def_i;
+}
+float HotPIDController::GetDefD() {
+	return def_d;
+}
+
+/**
+ * 	Modify PID
+ */
 void HotPIDController::SetP(float p) {
 	SetPID(p, GetI(), GetD());
 }
@@ -65,4 +86,50 @@ void HotPIDController::SetI(float i) {
 }
 void HotPIDController::SetD(float d) {
 	SetPID(GetP(), GetI(), GetD());
+}
+
+/**
+ * 	Modify Default PID
+ */
+void HotPIDController::SetDefPID(float p, float i, float d) {
+	def_p = p;
+	def_i = i;
+	def_d = d;
+}
+void HotPIDController::SetDefP(float p) {
+	SetDefPID(p, GetDefI(), GetDefD());
+}
+void HotPIDController::SetDefI(float i) {
+	SetDefPID(GetDefP(), i, GetDefD());
+}
+void HotPIDController::SetDefD(float d) {
+	SetDefPID(GetDefP(), GetDefI(), d);
+}
+
+/**
+ * 	Reset PID To Default Value
+ */
+void HotPIDController::ResetToDefPID() {
+	SetPID(def_p, def_i, def_d);
+}
+
+/**
+ * 	Set Dynamic PID
+ */
+void HotPIDController::DynamicPID(float range, float p, float i, float d) {
+	m_dynamicPIDs[range] = new PIDCoef(p, i, d);
+}
+
+/**
+ * 	Update PID For Update
+ */
+void HotPIDController::Update() {
+	for (std::map<float, PIDCoef*>::iterator it = m_dynamicPIDs.begin(); it != m_dynamicPIDs.end(); it++) {
+		if (it->first < fabs(GetError())) {
+			SetPID(it->second->GetP(), it->second->GetI(), it->second->GetD());
+			return;
+		}
+	}
+
+	ResetToDefPID();
 }
